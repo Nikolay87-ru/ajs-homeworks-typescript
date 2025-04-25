f920a6ba5d214aae0416d9478d478d5056c87f18
Neto test <nikolajeliseev834@gmail.com>
2025-04-25 20:34:51 +0300

    добавил тест

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,7 +1,25 @@
 import Cart from '../service/Cart';
+import Movie from '../domain/Movie';
 
 test('new card should be empty', () => {
   const cart = new Cart();
 
   expect(cart.items.length).toBe(0);
+});
+
+test('new Movie should be added', () => {
+  const cart = new Cart();
+  const movie = new Movie(
+    1010, 
+    "The Avengers", 
+    "Joss Whedon", 
+    999, 
+    "2012",
+    "USA",
+    "Avengers Assemble!",
+    ["fantasy", "action", "adventure"], 
+    "143 мин");
+
+  cart.add(movie);
+  expect(cart.items.length).toBe(1);
 });
