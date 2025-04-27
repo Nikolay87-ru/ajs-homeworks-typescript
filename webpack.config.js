64a7f89df93c488d5ac74d4da61c00474418b5b8
Neto test <nikolajeliseev834@gmail.com>
2025-04-27 18:09:52 +0300

    обновление конфигурации

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,40 @@
-import { resolve as _resolve, join } from "node:path";
+import { resolve as _resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import HtmlWebPackPlugin from "html-webpack-plugin";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 const config = {
+  mode: 'development',
   entry: "./src/index.ts",
-  resolve: {
-    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
-  },
   output: {
     path: _resolve(__dirname, "dist"),
     filename: "bundle.js",
+    clean: true
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".tsx", ".ts"]
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        exclude: /node_modules/,
         use: "ts-loader",
-      },
-      {
-        test: /\.html$/,
-        use: "html-loader",
+        exclude: /node_modules/
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-    ],
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html",
-    }),
-  ],
-  devtool: "inline-source-map",
-  devServer: {
-    static: {
-      directory: join(__dirname, "dist"),
-    },
-    compress: true,
-    port: 8080,
-    hot: true,
-  },
+      filename: "index.html",
+      inject: true
+    })
+  ]
 };
 
 export default config;
