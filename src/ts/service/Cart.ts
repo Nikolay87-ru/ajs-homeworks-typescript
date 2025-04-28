946d4c3f5f0589e78d60bb57be8584e07a5be21e
Neto test <nikolajeliseev834@gmail.com>
2025-04-28 19:45:51 +0300

    изменина логика, при добавлении создается копия объекта для item

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -4,35 +4,32 @@ export default class Cart {
   private _items: Buyable[] = [];
 
   add(item: Buyable): void {
-    const existingItem = this._items.find(
-      (existItem) => existItem.id === item.id
-    );
+    const existingItem = this._items.find(existItem => existItem.id === item.id);
 
     if (item.type === "digital") {
       if (!existingItem) {
-        this._items.push(item);
+        this._items.push({...item});
       }
       return;
     }
 
     if (existingItem) {
       existingItem.quantity += 1;
-      existingItem.price += item.price;
+      existingItem.price = item.price * existingItem.quantity;
     } else {
-      this._items.push(item);
+      this._items.push({...item});
     }
   }
 
   remove(id: number): void {
-    const index = this._items.findIndex((item) => item.id === id);
-    
+    const index = this._items.findIndex(item => item.id === id);
     if (index === -1) return;
 
     const item = this._items[index];
-
+    
     if (item.type === "product" && item.quantity > 1) {
       item.quantity -= 1;
-      item.price -= item.price / (item.quantity + 1);
+      item.price = (item.price / (item.quantity + 1)) * item.quantity;
     } else {
       this._items.splice(index, 1);
     }
