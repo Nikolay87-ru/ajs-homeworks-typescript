2a39d3793417b7554fc3de27107a7b218631a276
Neto test <nikolajeliseev834@gmail.com>
2025-04-30 14:42:44 +0300

    замена на item type

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,15 +3,16 @@ import Book from "./domain/Book";
 import MusicAlbum from "./domain/MusicAlbum";
 import Movie from "./domain/Movie";
 import Smartphone from "./domain/Smartphone";
+import { ItemType } from "./domain/Buyable";
 
 const cart = new Cart();
 console.log(cart.items);
 
 cart.add(
-  new Book(1001, "War and Piece", "Leo Tolstoy", 2000, 1225, "digital", 1)
+  new Book(1001, "War and Piece", "Leo Tolstoy", 2000, 1225, ItemType.Digital, 1)
 );
-cart.add(new MusicAlbum(1008, "Meteora", "Linkin Park", 900, "digital", 1));
-cart.add(new Smartphone(1020, "iPhone 13", 60000, "product", 1));
+cart.add(new MusicAlbum(1008, "Meteora", "Linkin Park", 900, ItemType.Digital, 1));
+cart.add(new Smartphone(1020, "iPhone 13", 60000, ItemType.Product, 1));
 cart.add(
   new Movie(
     1010,
@@ -23,7 +24,7 @@ cart.add(
     "Мстители, общий сбор!",
     ["фантастика", "боевик", "приключения"],
     "143 мин",
-    "digital",
+    ItemType.Digital,
     1
   )
 );
